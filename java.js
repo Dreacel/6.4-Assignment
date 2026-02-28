ecfe73ed1959a5ae22636123f1677fe35fa40434
Dreacel <dreacel05@gmail.com>
2026-02-27 21:20:50 -0600

    Update java.js

---
+++ java.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "lumi",
+    lastName: "nightgale",
+    age: 21,
+    fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+console.log
